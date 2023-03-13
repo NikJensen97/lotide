9df628b9bef3eb1b6851c895a8bc23cf8868b0ab
NikJensen97 <npjjensen@hotmail.com>
2023-03-13 16:05:22 -0700

    refactored code for middle

--- middle.js
+++ middle.js
@@ -1,21 +1,5 @@
-const eqArrays = function(actual, expected) {
-  let count = 0;
-  while(count < actual.length) {
-    if(actual[count] != expected[count]) {
-    return false;
-    }
-    count += 1;
-  }
-  return true
-};
-
-function assertEqualArrays(actual, expected) {
-  if(eqArrays(actual, expected)) {
-    console.log("Your arrays are equal!");
-  }
-  else console.log("The arrays were not equal");
-
-};
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual');
 
 function middle(array) {
   let newarray = [];
@@ -33,6 +17,5 @@ function middle(array) {
   }
 };
 
-assertEqualArrays(middle([1, 2, 3]), [2]);
-assertEqualArrays(middle([2, 2, 4, 5]), [2, 4]);
-assertEqualArrays(middle([1, 3, 5, 7, 9]), [5]);
+
+module.exports = middle;
