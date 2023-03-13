5e7ee99155285f89a5ebbc2f6852b66f68ce0dbd
NikJensen97 <npjjensen@hotmail.com>
2023-03-13 15:40:11 -0700

    refactored eqArrays to export its function

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`\u{1F601} \u{1F601} \u{1F601} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`\u{1F643} \u{1F643} \u{1F643} Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
+const assertEqual = require('./assertEqual');
 
 const eqArrays = function(actual, expected) {
   let count = 0;
@@ -18,8 +11,6 @@ const eqArrays = function(actual, expected) {
   return true
 };
 
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"]));
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3]));
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 
+
+module.exports = eqArrays;
