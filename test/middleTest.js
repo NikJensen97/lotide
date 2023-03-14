3ad9561f6ab3719d5947e7ca1ca4122d389c2433
NikJensen97 <npjjensen@hotmail.com>
2023-03-13 17:02:29 -0700

    integrated mocha/chai into middleTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,15 @@
-const middle = require('../middle');
+/* const middle = require('../middle');
 const assertArraysEqual = require('../assertArraysEqual');
 
 assertArraysEqual(middle([1, 2, 3]), [2]);
 assertArraysEqual(middle([2, 2, 4, 5]), [2, 4]);
 assertArraysEqual(middle([1, 3, 5, 7, 9]), [5]);
+*/
+const assert = require('chai').assert;
+const middle = require('../middle');
+
+describe("#middle", () => {
+  it("returns [2] for [1, 2, 3]", () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+});
